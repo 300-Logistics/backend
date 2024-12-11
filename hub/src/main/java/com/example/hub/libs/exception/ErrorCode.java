5683eb6075f802da495980849cf467199885a9d1
유현 <yh7119@gmail.com>
2024-12-11 12:45:29 +0900

    [#11] feat: HUB_NOT_FOUND 에러코드 생성

--- hub/src/main/java/com/example/hub/libs/exception/ErrorCode.java
+++ hub/src/main/java/com/example/hub/libs/exception/ErrorCode.java
@@ -24,6 +24,7 @@ public enum ErrorCode {
     ACCESS_DENIED(404, "접근 권한이 없습니다."),
     ORDER_NOT_FOUND(404, "주문을 찾을 수 없습니다."),
     PRODUCT_NOT_FOUND(404, "상품을 찾을 수 없습니다."),
+    HUB_NOT_FOUND(404, "허브를 찾을 수 없습니다."),
 
     /*  408 REQUEST_TIMEOUT : 요청에 대한 응답 시간 초과  */
     TIMEOUT_ERROR(408, "응답시간을 초과하였습니다."),
