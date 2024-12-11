01fc4841b684a6f52540c49c4293baa6f285051c
ksw4060 <gokorea1214@kakao.com>
2024-12-12 03:06:56 +0900

    [init] auth, gateway

--- auth/src/test/java/com/example/auth/AuthApplicationTests.java
+++ auth/src/test/java/com/example/auth/AuthApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.users;
+package com.example.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UsersApplicationTests {
+class AuthApplicationTests {
 
 	@Test
 	void contextLoads() {
