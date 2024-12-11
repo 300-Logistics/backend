a8013492b3d106e3faab032c3870b549a128d0a8
유현 <yh7119@gmail.com>
2024-12-11 12:45:06 +0900

    [#11] feat: 생성자, 수정자, 삭제자 일관되게 관리

--- hub/src/main/java/com/example/hub/domain/model/entity/BaseTimeEntity.java
+++ hub/src/main/java/com/example/hub/domain/model/entity/BaseTimeEntity.java
@@ -5,9 +5,7 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -32,11 +30,10 @@ public class BaseTimeEntity {
     private LocalDateTime deletedAt;
 
     @Setter
-    @CreatedBy
     @Column(name = "created_by", updatable = false)
     private UUID createdBy;
 
-    @LastModifiedBy
+    @Setter
     @Column(name = "updated_by")
     private UUID updatedBy;
 
