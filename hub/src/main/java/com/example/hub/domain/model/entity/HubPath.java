054024e4c15a3480689c929f1be702678e73590a
유현 <yh7119@gmail.com>
2024-12-12 13:06:40 +0900

    [#18] feat: 허브 이동경로 엔티티 생성

---
+++ hub/src/main/java/com/example/hub/domain/model/entity/HubPath.java
@@ -0,0 +1,40 @@
+package com.example.hub.domain.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+@Table(
+    name = "p_hub_path"
+)
+public class HubPath {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "start_hub_id", nullable = false)
+    private Hub startHub;
+
+    @ManyToOne
+    @JoinColumn(name = "end_hub_id", nullable = false)
+    private Hub endHub;
+
+    @Column(name = "distance", nullable = false)
+    private double distance; // 이동 거리 (km)
+
+    @Column(name = "duration", nullable = false)
+    private double duration; // 소요 시간 (hours)
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted;
+
+}
