9dbb012d409e42fb556e8984887751f027ab4c6c
yanJuicy <hanjub3@gmail.com>
2024-12-13 15:40:58 +0900

    [#24] feat: Order 엔터티에 수량, 배송 ID 필드 추가

--- order/src/main/java/com/example/order/domain/model/Order.java
+++ order/src/main/java/com/example/order/domain/model/Order.java
@@ -2,6 +2,7 @@ package com.example.order.domain.model;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -27,14 +28,25 @@ public class Order {
     private UUID customerId;
 
     @Column(nullable = false)
+    private int count;
+
+    @Column(nullable = false)
+    private UUID deliveryId;
+
+    @Column(nullable = false)
     private boolean isDeleted;
 
+    @Column
     private String requests;
 
-    public Order(UUID productId, UUID supplierId, UUID customerId, boolean isDeleted, String requests) {
+    @Builder
+    public Order(UUID productId, UUID supplierId, UUID customerId,
+                 int count, UUID deliveryId, boolean isDeleted, String requests) {
         this.productId = productId;
         this.supplierId = supplierId;
         this.customerId = customerId;
+        this.count = count;
+        this.deliveryId = deliveryId;
         this.isDeleted = isDeleted;
         this.requests = requests;
     }
