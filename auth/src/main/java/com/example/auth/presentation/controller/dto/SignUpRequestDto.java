b60cc18442249ff3aaf205dd64fc77a398b31d94
dmlal <opkl8781@naver.com>
2024-12-16 22:32:58 +0900

    [refactor] auth 아이디 비밀번호 정규식 제한 추가 #35

--- auth/src/main/java/com/example/auth/presentation/controller/dto/SignUpRequestDto.java
+++ auth/src/main/java/com/example/auth/presentation/controller/dto/SignUpRequestDto.java
@@ -4,14 +4,15 @@ import com.example.auth.domain.model.enums.UserRole;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 
 public record SignUpRequestDto(
 	@NotBlank
-	@Size(max = 16, message = "최대 16자")
+	@Pattern(regexp = "^[a-z0-9]{4,10}$", message = "소문자 a~z  숫자 0~9  최소 4자 최대 10자")
 	String username,
 	@NotBlank
-	@Size(min = 4, max = 16, message = "최소 4자 최대 16자")
+	@Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?~`]{8,15}$",
+	message = "대소문자 숫자 특수문자  최소 8자 최대 15자")
 	String password,
 	@NotBlank
 	@Email
