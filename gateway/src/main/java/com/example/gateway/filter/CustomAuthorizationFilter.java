373a0a699220287b81f62a931383d33de996bae2
dmlal <opkl8781@naver.com>
2024-12-17 00:49:12 +0900

    [feat] 게이트웨이 인증 인가 필터 구현 #37

---
+++ gateway/src/main/java/com/example/gateway/filter/CustomAuthorizationFilter.java
@@ -0,0 +1,50 @@
+package com.example.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import com.example.gateway.config.JwtProvider;
+import com.example.gateway.libs.exception.CustomException;
+import com.example.gateway.libs.exception.ErrorCode;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class CustomAuthorizationFilter implements GlobalFilter, Ordered {
+
+	private final JwtProvider jwtProvider;
+
+	public CustomAuthorizationFilter(JwtProvider jwtProvider) {
+		this.jwtProvider = jwtProvider;
+	}
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		String path = exchange.getRequest().getURI().getPath();
+
+		if (path.contains("/api/auth/signUp") || path.contains("/api/auth/signIn")) {
+			return chain.filter(exchange);
+		}
+
+		String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+
+		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+			throw new CustomException(ErrorCode.UNAUTHORIZED);
+		}
+
+		String token = authHeader.substring(7);
+		if (!jwtProvider.validateToken(token)) {
+			throw new CustomException(ErrorCode.FORBIDDEN);
+		}
+
+		return chain.filter(exchange);
+	}
+
+	@Override
+	public int getOrder() {
+		return Ordered.HIGHEST_PRECEDENCE + 1;
+	}
+}
