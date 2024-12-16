30c0c661ad1daabc427f5abb14efdac686f3eb53
yanJuicy <hanjub3@gmail.com>
2024-12-16 17:53:09 +0900

    [#24] feat: Order가 isDeleted=false인 것만 찾는 jpa 메소드 작성

--- order/src/main/java/com/example/order/domain/repository/OrderRepository.java
+++ order/src/main/java/com/example/order/domain/repository/OrderRepository.java
@@ -3,7 +3,11 @@ package com.example.order.domain.repository;
 import com.example.order.domain.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
+
+    Optional<Order> findByOrderIdAndDeletedAtNull(UUID orderId);
+
 }
