112c9586e25fa5bddfcf20df7b8aab1a75ed17ed
yanJuicy <hanjub3@gmail.com>
2024-12-16 17:53:42 +0900

    [#24] feat: OrderRequest에 deliverId 필드 추가, Put API에서 사용

--- order/src/main/java/com/example/order/presentation/request/OrderRequest.java
+++ order/src/main/java/com/example/order/presentation/request/OrderRequest.java
@@ -12,12 +12,14 @@ public class OrderRequest {
     private UUID productId;
     private int count;
     private String requests;
+    private UUID deliveryId;    // null 가능
 
-    public OrderRequest(UUID supplierId, UUID customerId, UUID productId, int count, String requests) {
+    public OrderRequest(UUID supplierId, UUID customerId, UUID productId, int count, String requests, UUID deliveryId) {
         this.supplierId = supplierId;
         this.customerId = customerId;
         this.productId = productId;
         this.count = count;
         this.requests = requests;
+        this.deliveryId = deliveryId;
     }
 }
